b9368f6a7458519ca37c6d916dd7767ad2497ade
Will Downey <willd20@vt.edu>
2022-06-27 16:03:11 -0400

    AForm for adding books

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 let myLibrary = [];
 
+let form = document.querySelector(".form-popup");
+
+
+
+let addButton = document.getElementById('add-btn');
+addButton.addEventListener("click", () => {
+    form.style.display = 'block';
+})
+
 function Book(title, datePublished) {
     this.title = title;
     this.datePublished = datePublished;
@@ -7,4 +16,8 @@ function Book(title, datePublished) {
 
 function addBookToLibrary(book) {
     myLibrary.push(book);
+}
+
+function displayLibrary(library) {
+
 }
