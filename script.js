48d4db26e628414c7c0b3db3795b99e3a458789a
Will Downey <willd20@vt.edu>
2022-06-27 16:23:16 -0400

    Submitting form records data in myLibrary array

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
 let myLibrary = [];
 
-let form = document.querySelector(".form-popup");
-
-
-
+let formContainer = document.querySelector(".form-popup");
+let form = document.querySelector("form");
+let submitBtn = document.getElementById("submit-btn");
 let addButton = document.getElementById('add-btn');
+let bookGrid = document.getElementById("book-grid");
+
+
 addButton.addEventListener("click", () => {
-    form.style.display = 'block';
+    formContainer.style.display = 'block';
 })
 
-function Book(title, datePublished) {
+function Book(title, author, numPages) {
     this.title = title;
-    this.datePublished = datePublished;
+    this.author = author;
+    this.numPages = numPages;
+}
+
+const addBook = (e) => {
+    e.preventDefault();
+    const newBook = getBookFromInput();
+    addBookToLibrary(newBook);
+    formContainer.style.display = 'none';
+    updateBookGrid()
+}
+
+const updateBookGrid = () => {
+
+}
+
+const resetBookGrid = () => {
+
 }
 
 function addBookToLibrary(book) {
@@ -21,3 +40,13 @@ function addBookToLibrary(book) {
 function displayLibrary(library) {
 
 }
+
+const getBookFromInput = () => {
+    const title = document.getElementById('book-title').value
+    const author = document.getElementById('book-author').value
+    const pages = document.getElementById('book-pages').value
+    return new Book(title, author, pages)
+}
+
+
+form.onsubmit = addBook;
